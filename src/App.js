8ce160d8a310d4b68ddb5032d643d95d2a904586
Roshith R <r.roshith@gmail.com>
2021-06-12 17:21:22 +0530

    flatten array

--- src/App.js
+++ src/App.js
@@ -6,22 +6,59 @@ const USERURL = "https://randomuser.me/api/?results=20";
 const getUsers = async () => {
   const response = await axios.get(USERURL);
   const { results } = response.data;
-  console.log(results);
   return results;
 };
 
 export default function App() {
   const [users, setUsers] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const getLocations = (users) => {
+    return users && users.map((el) => el.location);
+  };
   useEffect(
     () =>
       getUsers()
-        .then((users) => setUsers(users))
+        .then((users) => {
+          setUsers(users);
+          setLocations(getLocations(users));
+        })
         .catch((error) => console.error(error)),
     []
   );
+
+  let flatArray = {};
+  let flatTheArray = (obj, keyName = "location") => {
+    Object.keys(obj).forEach((el) => {
+      let newKey = `${obj[el]}_${keyName}`;
+      if (typeof obj[el] === "object") {
+        flatTheArray(obj[el], newKey);
+      } else {
+        flatArray[newKey] = obj[el];
+      }
+    });
+  };
+
+  flatTheArray(locations);
+  console.log(flatArray);
   return (
     <div className="App">
-      {users && users.map((user) => <pre>{JSON.stringify(user, null, 5)}</pre>)}
+      <table>
+        <tr>
+          <th>state</th>
+          <th>city</th>
+          <th>country</th>
+        </tr>
+
+        {users &&
+          users.map((user) => (
+            <tr>
+              {" "}
+              <td>{user.location.state}</td>
+              <td>{user.location.city}</td>
+              <td>{user.location.country}</td>
+            </tr>
+          ))}
+      </table>
     </div>
   );
 }
