4bfcee301555d1052564ebc31b3dd990739a843e
Roshith R <r.roshith@gmail.com>
2021-06-12 09:00:46 +0530

    fetching random user data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import "./styles.css";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
+const USERURL = "https://randomuser.me/api/?results=20";
+const getUsers = async () => {
+  const response = await axios.get(USERURL);
+  const { results } = response.data;
+  console.log(results);
+  return results;
+};
+
 export default function App() {
-  return <div className="App">ds</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(
+    () =>
+      getUsers()
+        .then((users) => setUsers(users))
+        .catch((error) => console.error(error)),
+    []
+  );
+  return (
+    <div className="App">
+      {users && users.map((user) => <pre>{JSON.stringify(user, null, 5)}</pre>)}
+    </div>
+  );
 }
